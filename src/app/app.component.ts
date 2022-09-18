404d766f0b4db83ce5cfd1a595e3ae5670152e23
Ria Pacheco <ria.pacheco@coldboretechnology.com>
2022-09-18 01:20:04 -0600

    Using public script tag first

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'tesseract-ocr-test';
+  ocrImage = '';
+  Tesseract!: any;
+  result = 'Recognizing... ';
+
+  constructor() {
+    this.test();
+  }
+
+
+  test() {
+    this.Tesseract.recognize(this.ocrImage).then(function(result: any) {
+      alert(result.text);
+    });
+  }
 }
